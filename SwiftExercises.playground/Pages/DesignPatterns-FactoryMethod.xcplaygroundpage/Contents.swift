4556d231808180753657cbcbc4fb884991d0fbb9
Ryan Hieber <floatingpoint@gmail.com>
2018-02-28 23:55:10 -0800

    Added Static Factory with Simlple Factory including better explanation.

---
+++ SwiftExercises.playground/Pages/DesignPatterns-FactoryMethod.xcplaygroundpage/Contents.swift
@@ -0,0 +1,129 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ # Factory Method
+ ## Objective
+ Implement the factory method pattern in Swift.  I wanted to implement an example where the created types take different initialization data which the factory passes to them.  I have encountered this problem numerous times before. One case that I can remember involved creating MIDI messages which are used in music software to communicate note on/off messages as well as control changes (in addition to other types of MIDI message).  This example is a simplified version of the problem and does not match the MIDI specification.
+ 
+ This example defines two types of Midi message: NoteMessage which contains a note value, and ControlMessage which contains a control number. Both types of midi message have data, and channel values.
+ 
+ There are two factory classes, one for each type of Midi Message. The factory is initialized with a channel value, and  createMessage() takes the data as an argument. The ControlFactory is also initizlized with a control value which defines the value that its control messages get.
+ 
+ ### Factory Method Definition:
+ "Define an interface for creating an object, but let subclasses decide which class to instantiate. The Factory method lets a class defer instantiation it uses to subclasses." (Gang Of Four)
+ 
+ #### When to use this pattern:
+ - Use the factory method if the business requirements are more than just product creation.
+ - If you need to abstract the factory itself as well as product creation.
+ - If you want to control product creation steps or configuration, and the steps are customized depending on the factory.
+ */
+
+// *************** The Messages  ***************
+protocol AbstractMidiMessage {
+    func getChannel() -> Int
+    func getData() -> Int
+    func getType() -> String
+}
+
+//Base MidiMessageBase class holds channel and data members which all midi messages contain.
+class MidiMessageBase {
+    var channel, data: Int
+    public init(channel:Int, data: Int) {
+        self.channel = channel
+        self.data = data
+    }
+    
+    public func getChannel() -> Int {
+        return channel;
+    }
+    
+    public func getData() -> Int {
+        return data;
+    }
+}
+
+class NoteMessage: MidiMessageBase, AbstractMidiMessage {
+    private var noteValue: Int
+    
+    init(channel: Int, data: Int, noteValue: Int) {
+        self.noteValue = noteValue
+        super.init(channel: channel, data: data)
+    }
+    
+    public func getType() -> String {
+        return "NoteMessage"
+    }
+    // NoteMessage specific stuff
+    public func getNote() -> Int {
+        return noteValue;
+    }
+}
+
+class ControlMessage: MidiMessageBase, AbstractMidiMessage {
+    private var controlValue: Int;
+    init(channel: Int, data: Int, controlValue: Int) {
+        self.controlValue = controlValue
+        super.init(channel: channel, data: data)
+    }
+    
+    func getType() -> String {
+        return "ControlMessage"
+    }
+    func getControlValue() -> Int {
+        return controlValue;
+    }
+}
+
+// *************** The Factories ******************
+protocol AbstractMidiMessageFactory {
+    func makeMessage(data: Int) -> AbstractMidiMessage
+}
+
+// Factory which makes note midi messages with a random note value assigned.
+class RandomNoteFactory: AbstractMidiMessageFactory {
+    private var channel: Int
+    init(channel: Int) {
+        self.channel = channel
+    }
+    func makeMessage(data: Int) -> AbstractMidiMessage {
+        let randomNote: Int = Int(arc4random_uniform(128))
+        return NoteMessage(channel: channel, data: data, noteValue: randomNote)
+    }
+}
+
+// A factory for control midi messages. The factory is initialized with a control number, and outputs control messages with this number.
+class ControlFactory: AbstractMidiMessageFactory {
+    private var channel: Int
+    private var controlValue: Int // controlValue for messages created by this factory.
+    init(channel: Int, controlValue: Int) {
+        self.channel = channel
+        self.controlValue = controlValue
+    }
+    func makeMessage(data: Int) -> AbstractMidiMessage {
+        return ControlMessage(channel: channel, data: data, controlValue: controlValue)
+    }
+}
+
+// # Usage:
+// Create a messageFactory variable and assign a RandomNoteFactory to it.
+var messageFactory: AbstractMidiMessageFactory = RandomNoteFactory(channel: 0)
+var message: AbstractMidiMessage = messageFactory.makeMessage(data: 5)
+// Upcast so we can get NoteMessage specific members
+let noteMessage: NoteMessage = message as! NoteMessage
+print("Type: \(message.getType()) Channel: \(message.getChannel()) Data: \(message.getData()) NoteValue: \(noteMessage.getNote())");
+
+// Switch the message factory to a ControlFactory
+messageFactory = ControlFactory(channel: 1, controlValue: 55)
+message = messageFactory.makeMessage(data: 10)
+// upcast so we can access ControlMessage specific members.
+let controlMessage: ControlMessage = message as! ControlMessage
+print("Type: \(message.getType()) Channel: \(message.getChannel()) Data: \(message.getData()) ControlValue: \(controlMessage.getControlValue())");
+
+// # Output:
+// Type: NoteMessage Channel: 0 Data: 5 NoteValue: 31
+// Type: ControlMessage Channel: 1 Data: 10 ControlValue: 55
+
+
+//: [Next](@next)
---
+++ SwiftExercises.playground/Pages/DesignPatterns-SimpleFactory.xcplaygroundpage/Contents.swift
@@ -0,0 +1,131 @@
+//: [Previous](@previous)
+import Foundation
+
+/* # SimpleFactory and Static Factory
+ ## Objective:
+ Implement the simple factory design pattern. This pattern abstracts out the creation details of an object. This simple pattern isn't really a GOF design pattern but is worth knowing about. For more complex cases use the Abstract Factory pattern.
+ 
+ ### Simple/Static Factory Definition:
+    Creates objects without exposing the instantiation logic to the client.
+    Refers to the newly created object through a common interface
+ 
+ #### When to use this pattern:
+ -When you need to abstract out creation details or configuration of an object but don't need to control.  Sometimes the factory keeps a reference to all the created objects or otherwise organizes them.
+ - When you want to control instanciation. e.g. you want to restrict the total number of instances, or otherwise limit the instances.  The factory can keep a count and deny creating new ones. A constructor approach can't do this.
+ - Encapsulation - When there are other classes to set up to properly create an object (e.g. database connection, file system connection). Rather than connecting to these with each created instance, a factory allows you to set this up once in the factory, and then create instances fromm there.
+ - Constructor disambiguation- If you have several constructors with similar signatures, it can be confusing for the client to choose the correct one.  A factory allows you to provide better names for the client.
+ 
+ #### When to use the Factory Method pattern instead (see the playground page DesignPatterns-FactoryMethod for an example)
+ Use the factory method if the business requirements are more than just product creation.
+ - If you need to abstract the factory itself as well as product creation.
+ - If you want to control product creation steps or configuration, and the steps are customized depending on the factory.
+ - If you want to create families or groups of objects use an Abstract Factory
+ */
+
+
+print("**** StaticFactory example ****")
+/********** Static Factory ***********/
+// Note we could also put the static members in another class called WidgetFactory. And it would behave the same. This is commonly done, for example in the java Logger/LoggerFactory classes.
+public class Widget {
+    var data: String
+    public static var instanceCount = 0
+    
+    public static func createInstance(data: String) -> Widget {
+        instanceCount += 1
+        return Widget(data: data)
+    }
+    
+    init(data: String) {
+        self.data = data
+    }
+}
+//## Usage
+var w1 = Widget.createInstance(data: "w1")
+var w2 = Widget.createInstance(data: "w2")
+
+print("Instance count: \(Widget.instanceCount)")
+
+/* ## Output
+ Instance count: 2
+ */
+
+
+print("\n\n**** SimpleFactory example ****")
+//********** Simple Factory ***********
+// In most cases you can probably use a static factory instead. It's simpler since you don't need to instantiate the factory itself.
+enum VehicleType {
+    case sedan, suv, truck
+}
+
+protocol Vehicle {
+    func drive()
+    var id: String {get set}
+}
+
+
+class Sedan: Vehicle {
+    public var id: String
+    init(id: String) {
+        self.id = id
+    }
+    
+    func drive() {
+        print("driving a sedan(id: \(id))")
+    }
+}
+class Suv: Vehicle {
+    public var id: String
+    init(id: String) {
+        self.id = id
+    }
+    func drive() {
+        print("driving a SUV(id: \(id))")
+    }
+}
+class Truck: Vehicle {
+    public var id: String
+    init(id: String) {
+        self.id = id
+    }
+    func drive() {
+        print("driving a truck(id: \(id))")
+    }
+}
+
+class VehicleFactory {
+    static var nextId: Int = 0;
+    static func CreateVehicle(type: VehicleType) -> Vehicle {
+        let id = String(VehicleFactory.nextId)
+        VehicleFactory.nextId += 1
+        switch type {
+        case .sedan:
+            return Sedan(id: id)
+        case .suv:
+            return Suv(id: id)
+        case .truck:
+            return Truck(id: id)
+        }
+    }
+}
+
+//## Usage
+let sedan = VehicleFactory.CreateVehicle(type: .sedan)
+let sedan2 = VehicleFactory.CreateVehicle(type: .sedan)
+let suv = VehicleFactory.CreateVehicle(type: .suv)
+let truck = VehicleFactory.CreateVehicle(type: .truck)
+
+sedan.drive()
+sedan2.drive()
+suv.drive()
+truck.drive()
+
+/*
+ ## Output
+ driving a sedan(id: 0)
+ driving a sedan(id: 1)
+ driving a SUV(id: 2)
+ driving a truck(id: 3)
+*/
+
+
+//: [Next](@next)
