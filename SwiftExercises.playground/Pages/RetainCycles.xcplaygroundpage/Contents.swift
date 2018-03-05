6c7844ce0c472ce9de413d198651463be4befce1
Ryan Hieber <floatingpoint@gmail.com>
2018-03-04 22:21:31 -0800

    Added an example to RetainCycles playground page

--- SwiftExercises.playground/Pages/RetainCycles.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/RetainCycles.xcplaygroundpage/Contents.swift
@@ -10,6 +10,87 @@ import Foundation
  
 */
 
+//#### Interclass retain cycle
+
+class PersonRetainCycle {
+    public var friends: Array<PersonRetainCycle> = []
+    private var name: String
+    
+    init(_ name: String) {
+        print("init: \(name)")
+        self.name = name
+    }
+    
+    deinit {
+        print("deinit: \(name)")
+    }
+}
+
+var bobWithCycle: PersonRetainCycle? = PersonRetainCycle("BobWithCycle")
+var aliceWithCycle: PersonRetainCycle? = PersonRetainCycle("AliceWithCycle")
+
+bobWithCycle?.friends.append(aliceWithCycle!)
+aliceWithCycle?.friends.append(bobWithCycle!)
+
+bobWithCycle = nil
+aliceWithCycle = nil
+print("\n")
+/* #### Output:
+ Notice that the deinit never gets called
+ 
+ init: Bob
+ init: Alice
+ 
+ We can fix/avoid this in two ways. The first way is to remember to clear out the friends array when we are destroying a person.
+ bob.friends.RemoveAll()
+ bob = nil
+ 
+ 
+ We can also wrap the person objects held in the array with wrapper class/struct that holds a weak reference to a person.
+ */
+
+struct WeakPerson {
+    public weak var person: Person?
+    
+    init(_ person: Person) {
+        self.person = person
+    }
+}
+
+class Person {
+    public var friends: Array<WeakPerson> = []
+    private var name: String
+    
+    init(_ name: String) {
+        print("init: \(name)")
+        self.name = name
+    }
+    
+    deinit {
+        print("deinit: \(name)")
+    }
+}
+
+var bob: Person? = Person("Bob")
+var alice: Person? = Person("Alice")
+
+bob?.friends.append(WeakPerson(alice!))
+alice?.friends.append(WeakPerson(bob!))
+
+bob = nil
+alice = nil
+print("\n")
+
+/* #### Output:
+ Notice that now the deinits get called
+ 
+ init: Bob
+ init: Alice
+ deinit: Bob
+ deinit: Alice
+ */
+
+
 //#### Closure retain cycle
 // KrakenRetainCycle captures a strong reference to self in the notificationObserver closure.  de-init will never get called because this captured reference will always keep the retain count for the parent object at least 1.
 class KrakenRetainCycle {
@@ -18,10 +99,12 @@ class KrakenRetainCycle {
         notificationObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "humanEnteredKrakensLair"), object: nil, queue: .main) { _ in
             self.eatHuman()
         }
+        print("init: KrakenWithCycle")
     }
     
     deinit {
         NotificationCenter.default.removeObserver(notificationObserver!)
+        print("deinit: KrakenWithCycle")
     }
     
     public func eatHuman() -> Void {
@@ -29,11 +112,16 @@ class KrakenRetainCycle {
     }
 }
 
-//#### Usage:
-var kraken = KrakenRetainCycle()
-print("kraken retain count: \(CFGetRetainCount(kraken))")
+var krakenWithCycle: KrakenRetainCycle? = KrakenRetainCycle()
+krakenWithCycle = nil
+print("\n")
 
-/* adding [weak self] to the closure arguments means the closure now keeps a weak reference, which doesn't increase the retain count on self.
+/* #### Output:
+ Notice that the deinit doesn't get called
+ init: Kraken
+ 
+ 
+ adding [weak self] to the closure arguments means the closure now keeps a weak reference, which doesn't increase the retain count on self.
  */
 class KrakenWithWeakSelf {
     var notificationObserver: NSObjectProtocol? = nil
@@ -41,18 +129,59 @@ class KrakenWithWeakSelf {
         notificationObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "humanEnteredKrakensLair"), object: nil, queue: .main) { [weak self] (notification) in
             self?.eatHuman() // weak variables are optional
         }
+        print("init: KrakenWithWeakSelf")
     }
     
     deinit {
         NotificationCenter.default.removeObserver(notificationObserver!)
+        print("deinit: KrakenWithWeakSelf")
+
     }
     
     public func eatHuman() -> Void {print("eatHuman()")}
 }
-//#### Usage:
-let krakenWithWeakSelf = KrakenWithWeakSelf()
-print("kraken retain count: \(CFGetRetainCount(krakenWithWeakSelf))")
 
+var krakenWithWeakSelf: KrakenWithWeakSelf? = KrakenWithWeakSelf()
+krakenWithWeakSelf = nil
 
+/* #### Output:
+ Now the deinit is called
 
+ init: KrakenWithWeakSelf
+ deinit: KrakenWithWeakSelf
+ 
+*/
+/*
+protocol GodzillaListener {
+    func onAteSomebody()
+}
+
+class SomeListener: GodzillaListener {
+    public func onAteSomebody() {
+        print("Godzilla ate somebody")
+    }
+}
+ 
+class GodzillaWithCycle : GodzillaListener {
+    private var maxAge: Int = 33
+    
+    public var listener: GodzillaListener? = {
+        
+    }
+    
+    init() {
+        print("init: GodzillaWithCycle")
+        //listener = self
+    }
+    deinit {
+        print("deinit: GodzillaWithCycle")
+    }
+   /* public func onAteSomebody() {
+        print("Godzilla ate somebody")
+    }*/
+}
+
+var godzilla: GodzillaWithCycle? = GodzillaWithCycle()
+godzilla = nil
+*/
 //: [Next](@next)
