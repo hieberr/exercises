67b50977a7f75376290d876a86141faddfc616b7
Ryan Hieber <floatingpoint@gmail.com>
2018-03-25 23:55:48 -0700

    Adding Visitor design pattern definition

---
+++ SwiftExercises.playground/Pages/DesignPatterns-Visitor.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Visitor Design Pattern (Surrogate)
+ ## Objective:
+ Implement the Visitor Pattern
+ 
+ #### Definition:
+ ![Uml](VisitorUml.jpg)
+ gof definition: Represent an operation to be performed on elements of an object structure. Visitor lets you define a new operation without changing the classes of the elements on which it operates.
+ 
+ #### Use the visitor pattern when:
+- many unrelated operations on an object structure are required,
+- the classes that make up the object structure are known and not expected to change,
+- new operations need to be added frequently,
+- an algorithm involves several classes of the object structure, but it is desired to manage it in one single location,
+- an algorithm needs to work across several independent class hierarchies.
+ 
+ A drawback to this pattern, however, is that it makes extensions to the class hierarchy more difficult, as new classes typically require a new visit method to be added to each visitor.
+ #### Specific Problems and Implementations
+ 
+ ## Example:
+ */
+
+
+//: #### Usage
+
+
+
+//: #### Output
+/*
+ 
+ */
+//: [Next](@next)
+
+
+
+
+
+
+
+
