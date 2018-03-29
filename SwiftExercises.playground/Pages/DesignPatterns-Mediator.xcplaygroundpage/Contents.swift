aa7b177ada810e015ee56ba3b80348e5b4224c2b
Ryan Hieber <floatingpoint@gmail.com>
2018-03-28 23:59:49 -0700

    Adding mediator

---
+++ SwiftExercises.playground/Pages/DesignPatterns-Mediator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Mediator Design Pattern
+ ## Objective:
+ Implement the Mediator Pattern
+ 
+ #### Definition:
+ ![Uml]()
+ 
+ #### Specific Problems and Implementations
+ 
+ ## Example:
+ */
+
+
+//: #### Usage
+
+
+
+//: #### Output
+/*
+ 
+ */
+//: [Next](@next)
+
+
+
+
+
+
+
+
