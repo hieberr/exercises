1d6cc31d413053dcad9a5eebd855e8f972fbf5fd
Ryan Hieber <floatingpoint@gmail.com>
2018-04-06 20:20:52 -0700

    Adding Example for composite pattern

--- SwiftExercises.playground/Pages/DesignPatterns-Composite.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/DesignPatterns-Composite.xcplaygroundpage/Contents.swift
@@ -27,16 +27,140 @@ import Foundation
  The Composite design pattern emphasizes uniformity over type safety.
  
  ## Example:
+ ![ExampleUml](CompositeExampleUml.png)
  */
+public protocol Employee {
+    func isEqualTo(_ other: Employee) -> Bool
+    func add(employee: Employee)
+    func remove(employee: Employee)
+    func getChild(index: Int) -> Employee?
+    func getName() -> String
+    func getSalary() -> Double
+    func printDescription(tabs: String)
+}
 
+public class Manager : Employee {
+    private var name: String
+    private var salary: Double
+    private var employees: Array<Employee> = Array<Employee>()
+    
+    public init(name: String, salary: Double){
+        self.name = name
+        self.salary = salary
+    }
+    
+    public func isEqualTo(_ other: Employee) -> Bool {
+        return name == other.getName()
+    }
+    
+    public func add(employee: Employee) {
+        employees.append(employee)
+    }
+    
+    public func remove(employee: Employee) {
+        if let index = employees.index( where: {$0.isEqualTo(employee) }) {
+            employees.remove(at: index)
+        }
+    }
+    
+    public func getChild(index: Int) -> Employee? {
+        if index < 0 || index > employees.count - 1 {
+            return nil
+        }
+        return employees[index]
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+    
+    public func getSalary() -> Double {
+        return salary
+    }
+    
+    public func printDescription(tabs: String) {
+        print("\(tabs)Name: \(name)")
+        print("\(tabs)Salary: \(salary)")
+        print("\(tabs)-----------------------")
+        
+        for employee in employees {
+            employee.printDescription(tabs: tabs + "\t")
+        }
+    }
+}
+
+// Developer is a leaf node, so it doesn't keep track of any employees.
+public class Developer : Employee {
+    private var name: String
+    private var salary: Double
+    
+    public init(name: String, salary: Double){
+        self.name = name
+        self.salary = salary
+    }
+    
+    public func isEqualTo(_ other: Employee) -> Bool {
+        return name == other.getName()
+    }
+    
+    public func add(employee: Employee) {
+    }
+    
+    public func remove(employee: Employee) {
+    }
+    
+    public func getChild(index: Int) -> Employee? {
+        return nil
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+    
+    public func getSalary() -> Double {
+        return salary
+    }
+    
+    public func printDescription(tabs: String) {
+        print("\(tabs)Name: \(name)")
+        print("\(tabs)Salary: \(salary)")
+        print("\(tabs)-----------------------")
+    }
+}
 
 //: #### Usage
+let manager1 : Employee  = Manager(name: "Bill", salary: 200000)
+manager1.add(employee: Developer(name: "John", salary: 100000))
+manager1.add(employee: Developer(name: "David", salary: 150000))
 
+let manager2 : Employee  = Manager(name: "Bob", salary: 200000)
+manager2.add(employee: Developer(name: "Frank", salary: 150000))
 
+let generalManager : Employee = Manager(name: "Rich", salary: 200000)
+generalManager.add(employee: manager1)
+generalManager.add(employee: manager2)
 
+generalManager.printDescription(tabs: "")
 //: #### Output
 /*
- 
+ Name: Rich
+ Salary: 200000.0
+ -----------------------
+    Name: Bill
+    Salary: 200000.0
+    -----------------------
+        Name: John
+        Salary: 100000.0
+        -----------------------
+        Name: David
+        Salary: 150000.0
+        -----------------------
+    Name: Bob
+    Salary: 200000.0
+    -----------------------
+        Name: Frank
+        Salary: 150000.0
+        -----------------------
  */
 //: [Next](@next)
 
