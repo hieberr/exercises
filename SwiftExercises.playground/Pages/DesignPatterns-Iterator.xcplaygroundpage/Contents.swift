09d29c8c9024b49a90167124ec9cf10eb6633069
Ryan Hieber <floatingpoint@gmail.com>
2018-04-19 23:40:33 -0700

    Added Iterator Pattern

---
+++ SwiftExercises.playground/Pages/DesignPatterns-Iterator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Iterator Design Pattern
+ ## Objective:
+ Implement the Iterator Pattern
+ 
+ #### Definition:
+ ![Uml](IteratorUml.png)
+ The iterator pattern is used to traverse and access a container of elements sequentially without knowing it's representation (e.g. the datastructure of the aggregate could be an array, a heap, a binary tree.. etc)
+ 
+ - Define a seperate itorator object that encapsulates accessing and traversing an aggregate object.
+ 
+ #### Specific Problems and Implementations
+ 
+ ## Example:
+ */
+
+
+//: #### Usage
+
+
+
+//: #### Output
+/*
+ 
+ */
+//: [Next](@next)
+
