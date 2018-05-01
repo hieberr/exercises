0f52afd1654858258c0fc65cdc2e36b808ef22c2
Ryan Hieber <floatingpoint@gmail.com>
2018-05-01 00:30:25 -0700

    Added Memento Example

--- SwiftExercises.playground/Pages/DesignPatterns-Memento.xcplaygroundpage/Contents.swift
+++ SwiftExercises.playground/Pages/DesignPatterns-Memento.xcplaygroundpage/Contents.swift
@@ -23,17 +23,95 @@ import Foundation
  
  #### Specific Problems and Implementations
  
+
  ## Example:
  */
+//The memento
+struct GameState {
+    public var score, level : Int
+    public var chapter: String
+    
+    init(score: Int, level: Int, chapter: String) {
+        self.score = score; self.level = level; self.chapter = chapter;
+    }
 
+}
+
+// The originator
+struct Game {
+    public var score = 0, level = 0
+    public var chapter: String = ""
+    
+    public func SaveGame() -> GameState {
+        return GameState(score: score, level: level, chapter: chapter)
+    }
+    
+    public mutating func loadGame(from: GameState) {
+        score = from.score; level = from.level; chapter = from.chapter
+    }
+    
+    public func PrintGameState() {
+        print("Beginning game at chapter: \(chapter)-level: \(level), score: \(score)")
+    }
+    
+    
+}
+
+// The caretaker
+struct GameController {
+    private var savedStates: Array<GameState> = Array<GameState>()
+    
+    public mutating func store(state: GameState) {
+        savedStates.append(state)
+    }
+    
+    public func get(index: Int) -> GameState? {
+        return savedStates[index]
+    }
+}
 
 //: #### Usage
+var gameController = GameController()
+var game = Game()
 
+game.score += 200;
+game.level = 1;
+game.chapter = "Bubble World"
+gameController.store(state: game.SaveGame())
 
+game.score += 300;
+game.level = 2;
+game.chapter = "Bubble World"
+gameController.store(state: game.SaveGame())
+
+game.score += 500;
+game.level = 3;
+game.chapter = "Bubble World"
+gameController.store(state: game.SaveGame())
+
+game.score += 300;
+game.level = 1;
+game.chapter = "Lava World"
+gameController.store(state: game.SaveGame())
+
+game.score += 800;
+game.level = 2;
+game.chapter = "Lava World"
+gameController.store(state: game.SaveGame())
+
+game.loadGame(from: gameController.get(index: 2)!)
+game.PrintGameState()
+game.loadGame(from: gameController.get(index: 3)!)
+game.PrintGameState()
+
+game.loadGame(from: gameController.get(index: 0)!)
+game.PrintGameState()
 
 //: #### Output
 /*
- 
+ Beginning game at chapter: Bubble World-level: 3, score: 1000
+ Beginning game at chapter: Lava World-level: 1, score: 1300
+ Beginning game at chapter: Bubble World-level: 1, score: 200
  */
 //: [Next](@next)
 
